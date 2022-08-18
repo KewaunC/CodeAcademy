f0f0f492f51aef2ecfca3588c642d851622a615e
Kewaun Cain <kewaunscain@gmail.com>
2022-08-17 20:55:03 -0400

    Code Academy

---
+++ helperFunctions.js
@@ -0,0 +1,11 @@
+const totalCost = costOfMonitors(5, 4)
+
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+
+function costOfMonitors(rows, columns){
+  return monitorCount(rows, columns) *200
+}
+
+console.log(totalCost)
