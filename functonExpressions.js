67f76efd96f818e4d7d4e16186ad862909950312
Kewaun Cain <kewaunscain@gmail.com>
2022-08-17 20:59:26 -0400

    Code Academy

--- functonExpressions.js
+++ functonExpressions.js
@@ -6,4 +6,17 @@ const plantNeedsWater = function(day){
   }
 } 
 
+// Cam we make this into an arrow function?
 console.log(plantNeedsWater('Tuesday'))
+
+const plantNeedsWater = (day) => {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+
+//can I shorten it even more?
+const plantNeedsWater = day => day === 'Wednesday' ? true : false;
