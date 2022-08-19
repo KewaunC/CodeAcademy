2e9ea344f751dc8b466a1d937ea9b4ee86eba238
Kewaun Cain <kewaunscain@gmail.com>
2022-08-18 20:09:14 -0400

    Scope and Block

--- Scope/scopeAndBlock.js
+++ Scope/scopeAndBlock.js
@@ -6,3 +6,13 @@ function logCitySkyline(){
 }
 
 console.log(logCitySkyline())
+
+const satellite = 'The Moon'
+const galaxy = 'The Milky Way'
+const stars = 'North Star'
+
+function callMyNightSky(){
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+
+console.log(callMyNightSky())
