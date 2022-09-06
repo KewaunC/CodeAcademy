e1d2e984fb8e8c0822b830dc2063ac51f4096fd9
Kewaun Cain <kewaunscain@gmail.com>
2022-09-06 14:03:10 -0400

    Update

---
+++ whaleTalk.js
@@ -0,0 +1,18 @@
+const input = "turpentine and turtles".toLowerCase();
+const vowels = ["a", "e", "i", "o", "u"];
+
+let resultArray = [];
+// let resultString = resultArray.join().toUpperCase()
+
+for (let i = 0; i < input.length; i++) {
+  if (input[i] === "e" || input[i] === "u") {
+    resultArray.push(input[i]);
+  }
+   for (let v = 0; v < vowels.length; v++) {
+    if (input[i] === vowels[v]) {
+      resultArray.push(vowels[v]);
+    }
+  }
+} 
+
+console.log(resultArray.join('').toUpperCase());
